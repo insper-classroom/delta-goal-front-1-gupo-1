cea99cfa71bc2567f0ae9bca547f1f60125da821
rxdrigues <rodriguesrgbr@gmail.com>
2023-12-08 18:24:12 -0300

    funções dos campos

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import json
 import plotly.express as px
 import pandas as pd
+import streamlit as st
+from PIL import Image, ImageDraw, ImageFont
 
 
 # Função para verificar em qual tempo o jogo está
@@ -196,3 +198,81 @@ def top_5_cruzamentos(json, time_id):
         dicionario_crescente[i] = top_cruzamentos[i]
 
     return dicionario_crescente
+
+# Campos
+def imagem_campo_quebra(json):
+    dict_quebra = json
+    campo = Image.open('assets\campo2.png')
+    campo_cruzamentos = campo.resize((1200, 800))
+    draw = ImageDraw.Draw(campo_cruzamentos)
+
+    zonas = {
+        "Zona 1 - B": ( 0 , 0),
+        "Zona 1 - A": (200, 0),
+        "Zona 2":     (400, 0),
+        "Zona 3 - A": (800, 0),
+        "Zona 3 - B": (1000, 0)
+    }
+
+    dict_porcentagem = {}
+
+    # calcular a porcentagem de presença em cada zona
+    for quebra in dict_quebra.values():
+        for info, valor in quebra.items():
+            if info == "zona":
+                zona = valor
+                if zona in dict_porcentagem:
+                    dict_porcentagem[zona] += 1
+                else:
+                    dict_porcentagem[zona] = 1
+
+    # desenhar as porcentagens no campo
+
+    for zona, valor in dict_porcentagem.items():
+        if zona == "Zona 2":
+            posicao = (zonas[zona][0] + 65, zonas[zona][1] + 170)
+            draw.text(posicao, f"{(valor / sum(dict_porcentagem.values())):.2f}%", (255, 255, 255), font_size=35)
+        else:
+            posicao = (zonas[zona][0] + 80, zonas[zona][1] + 170)
+            draw.text(posicao, f"{(valor / sum(dict_porcentagem.values())):.2f}%", (255, 255, 255), font_size=35)
+
+    return st.image(campo_cruzamentos)
+
+def imagem_campo_cruzamentos(json):
+    frequencia_cada_time = json
+
+    sub_zonas = {
+        "zona_1": {
+                "D1.1": (262, 126), "D1.2": (261,  0 ), "D2.1": (131,  0 ), "D2.2": ( 0 ,  0 ), "D3": ( 0 , 126),
+                "E1.1": (262, 378), "E1.2": (262, 634), "E2.1": (131, 634), "E2.2": ( 0 , 634), "E3": ( 0 , 504)
+        },
+        "zona_3": {
+                "D1.1": (786, 126), "D1.2": (786, 0), "D2.1": (917, 0), "D2.2": (1048, 0), "D3": (917, 126),
+                "E1.1": (786, 378), "E1.2": (786, 634), "E2.1": (917, 634), "E2.2": (1048, 634), "E3": (917 , 504),
+            }}
+
+    campo = Image.open('assets\campo1.png')
+    campo_cruzamentos = campo.resize((1190, 800))
+    draw = ImageDraw.Draw(campo_cruzamentos)
+
+    base = 1
+    for frequencia in frequencia_cada_time:
+        if base == 1:
+            for zona in frequencia:
+                if zona == "D3" or zona == "E3":
+                    posicao = (sub_zonas["zona_3"][zona][0] + 65, sub_zonas["zona_1"][zona][1] + 75)
+                    draw.text(posicao, f"{(frequencia[zona] / sum(frequencia.values())):.2f}%", (255, 255, 255), font_size=35)
+                else:
+                    posicao = (sub_zonas["zona_3"][zona][0] + 15, sub_zonas["zona_3"][zona][1] + 80)
+                    draw.text(posicao, f"{(frequencia[zona] / sum(frequencia.values())):.2f}%", (255, 255, 255), font_size=35)
+            base += 1
+        else:
+            for zona in frequencia:
+                if zona == "D3" or zona == "E3":
+                    posicao = (sub_zonas["zona_1"][zona][0] + 121, sub_zonas["zona_1"][zona][1] + 75)
+                    draw.text(posicao, f"{(frequencia[zona] / sum(frequencia.values())):.2f}%", (255, 255, 255), font_size=35)
+                else:
+                    posicao = (sub_zonas["zona_1"][zona][0] + 40, sub_zonas["zona_1"][zona][1] + 70)
+                    draw.text(posicao, f"{(frequencia[zona] / sum(frequencia.values())):.2f}%", (255, 255, 255), font_size=35)
+
+    return st.image(campo_cruzamentos)
